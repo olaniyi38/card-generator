80cd6223faf3855f32c47303437528ed2b7bd0ed
sodiq <sodiqalao38@gmail.com>
2024-08-29 20:45:09 +0100

    env bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   define: {
-    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+    __APP_ENV__: process.env.VITE_ACCESS_KEY,
   },
 });
